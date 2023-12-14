e0770eec277d922fd1e908479e3f4070fde79c5a
CJ0423 <118946921+CJ0423@users.noreply.github.com>
2023-12-14 15:53:07 +0800

    ˋ

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,11 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css',
+                'resources/css/app.scss',
+                'resources/css/styleguide.css',
+                'resources/css/globals.css',
+                'resources/css/style.scss',
+                'resources/css/frontpage.scss',
                 'resources/js/app.js',
             ],
             refresh: true,
