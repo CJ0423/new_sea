81942a84adb45f858676d83b331c90481670e466
howard10203 <83261913+howard10203@users.noreply.github.com>
2023-12-20 03:58:58 +0800

    1

--- resources/js/banner.js
+++ resources/js/banner.js
@@ -1,24 +1,70 @@
-var all = document.querySelector('.input-all'); //全部
-var down = document.querySelector('.input-down'); //下架
+var input_all = document.querySelector('.input-all'); //全部
+var input_down = document.querySelector('.input-down'); //下架
 
 var banner_superior = document.querySelectorAll('.banner-superior'); //已上架
 var banner_down = document.querySelectorAll('.banner-down'); //已下架
 var banner_prepare = document.querySelectorAll('.banner-prepare'); //未上架
 
 
-all.onclick = function () {
-    all.style.color = "#6ebe49";
-    down.style.color = "#000";
+input_all.onclick = function () {
+    input_all.style.color = "#6ebe49";
+    input_down.style.color = "#000";
 
-    banner_superior.forEach((element) => {element.style.display = ""});
-    banner_down.forEach((element) => {element.style.display = ""});
-    banner_prepare.forEach((element) => {element.style.display = ""});
+    banner_superior.forEach((element) => { element.style.display = "" });
+    banner_down.forEach((element) => { element.style.display = "" });
+    banner_prepare.forEach((element) => { element.style.display = "" });
 }
-down.onclick = function () {
-    all.style.color = "#000";
-    down.style.color = "#6ebe49";
+input_down.onclick = function () {
+    input_all.style.color = "#000";
+    input_down.style.color = "#6ebe49";
 
-    banner_superior.forEach((element) => {element.style.display = "none"});
-    banner_down.forEach((element) => {element.style.display = ""});
-    banner_prepare.forEach((element) => {element.style.display = "none"});
+    banner_superior.forEach((element) => { element.style.display = "none" });
+    banner_down.forEach((element) => { element.style.display = "" });
+    banner_prepare.forEach((element) => { element.style.display = "none" });
+}
+
+
+
+
+// let down = Array.from(document.querySelectorAll(".button-down")) //下架
+// console.log(down)
+// let prompt_down = document.querySelector('.prompt-box-down') //下架提示框
+// console.log(prompt_down)
+
+let del = Array.from(document.querySelectorAll(".button-delete")) //刪除
+console.log(del)
+let prompt_del = document.querySelector('.prompt-box-delete') //刪除提示框
+console.log(prompt_del)
+
+del.forEach(element => {
+    element.addEventListener("click", delcheck)
+})
+
+let cancel_del = document.querySelector(".cancel-delete")
+cancel_del.addEventListener("click", msgClose)
+
+let confirm_del = document.querySelector(".confirm-delete")
+
+confirm_del.addEventListener("click", Sendmsg)
+
+let delTarget = 0
+function delcheck () {
+    console.log(this.dataset.key)
+    delTarget = this.dataset.key
+    prompt_del.style.display = 'flex'
+}
+function msgClose () {
+
+    console.log("text")
+    prompt_del.style.display = 'none'
+}
+function Sendmsg () {
+    console.log(this)
+    let myForm = document.getElementById("delete-form")
+    console.log(delTarget)
+    myForm.action = delTarget
+
+    console.log(myForm)
+    myForm.submit()
+    // prompt.style.display = 'none'
 }
--- resources/views/seageat/Recommend.blade.php
+++ resources/views/seageat/Recommend.blade.php
@@ -2,38 +2,38 @@
 
 
 @section('title')
-後端管理-推薦通路管理
+    後端管理-推薦通路管理
 @endsection
 
 @section('css')
-@vite(['resources/css/recommend.scss'])
+    @vite(['resources/css/recommend.scss'])
 @endsection
 @section('cut')
-      <!-- 以下分割 -->
-      <div class="size16">推薦通路管理</div>
-      <div class="border border-0 card">
+    <!-- 以下分割 -->
+    <div class="size16">推薦通路管理</div>
+    <div class="border border-0 card">
 
         <!-- 提示訊息 -->
         <!-- <div class="prompt-box-down">
-          <div class="prompt">
-            <p class="size14">確定要下架嗎?</p>
-            <div>
-              <input class="border confirm" type="button" value="確認">
-              <input class="border cancel" type="button" value="取消">
-            </div>
-          </div>
-        </div> -->
+              <div class="prompt">
+                <p class="size14">確定要下架嗎?</p>
+                <div>
+                  <input class="border confirm" type="button" value="確認">
+                  <input class="border cancel" type="button" value="取消">
+                </div>
+              </div>
+            </div> -->
 
         <div class="frame-2">
-          <div class="size12">推薦通路列表</div>
-          <div>
-              <a href={{route('RecommendEstablish')}}  class="button-establish">
+            <div class="size12">推薦通路列表</div>
+            <div>
+                <a href={{ route('RecommendEstablish') }} class="button-establish">
 
 
-                <img class="icon-outline-plus" src={{asset("img/icon-outline-plus-22.svg")}} />
-                <div class="text-2"> 建立通路</div>
-              </a>
-          </div>
+                    <img class="icon-outline-plus" src={{ asset('img/icon-outline-plus-22.svg') }} />
+                    <div class="text-2"> 建立通路</div>
+                </a>
+            </div>
         </div>
 
 
@@ -42,55 +42,55 @@
         <form id="delete-form" method="post">
             @method('DELETE')
             @csrf
-          <div class="frame-3">
-            <table class="table">
-              <thead>
-                <tr>
-                  <th scope="col">編號</th>
-                  <th scope="col">推薦通路Logo</th>
-                  <th scope="col">推薦通路連結</th>
-                  <th scope="col">操作</th>
-                </tr>
-              </thead>
-              <tbody>
-                @foreach ($recommendData as $index=>$item )
-                <tr class="border-bottom">
-                    <th scope="row">{{$index+1}}</th>
-                    <td><img src='{{asset('storage/'.$item->logo_url)}}'alt=""></td>
-                    <td>{{$item->logo_link}}</td>
-                    <td>
-                      <div class="operate">
-                          <a href=" {{ route('RecommendRevise',['id'=>$item->id])}}" class="border border-0 button-edit">編輯</a>
-                          <input data-key="{{route('destoryRecommend',['id'=>$item->id])}}" type="button" class="border button-delete" value="刪除">
+            <div class="frame-3">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">編號</th>
+                            <th scope="col">推薦通路Logo</th>
+                            <th scope="col">推薦通路連結</th>
+                            <th scope="col">操作</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($recommendData as $index => $item)
+                            <tr class="border-bottom">
+                                <th scope="row">{{ $index + 1 }}</th>
+                                <td><img src='{{ asset('storage/' . $item->logo_url) }}'alt=""></td>
+                                <td>{{ $item->logo_link }}</td>
+                                <td>
+                                    <div class="operate">
+                                        <a href=" {{ route('RecommendRevise', ['id' => $item->id]) }}"
+                                            class="border border-0 button-edit">編輯</a>
+                                        <input data-key="{{ route('destoryRecommend', ['id' => $item->id]) }}" type="button"
+                                            class="border button-delete" value="刪除">
 
 
-                          {{-- 現在已經到了 可以更新的地方了 但是還沒有把資料傳過去 --}}
-                      </div>
-                  </td>
-                  </tr>
-                @endforeach
+                                        {{-- 現在已經到了 可以更新的地方了 但是還沒有把資料傳過去 --}}
+                                    </div>
+                                </td>
+                            </tr>
+                        @endforeach
 
-              </tbody>
-            </table>
-          </div>
+                    </tbody>
+                </table>
+            </div>
         </form>
         <div class="prompt-box-down">
             <div class="prompt">
-              <p class="size14">確定要捨棄嗎?</p>
-              <div>
-                <input class="border confirm" type="button" value="確認">
-                <input  class="border cancel" type="button" value="取消">
-              </div>
+                <p class="size14">確定要捨棄嗎?</p>
+                <div>
+                    <input class="border confirm" type="button" value="確認">
+                    <input class="border cancel" type="button" value="取消">
+                </div>
             </div>
-          </div>
-      </div>
-      <!-- 以上分割 -->
+        </div>
+    </div>
+    <!-- 以上分割 -->
 @endsection
 
 @section('js')
-<script src={{asset("./thedatepicker-master/dist/the-datepicker.js")}}></script>
-<script src={{asset("./thedatepicker-master/dist/dataPicker.js")}}></script>
-@vite(['resources/js/recommend.js',])
-
-
+    <script src={{ asset('./thedatepicker-master/dist/the-datepicker.js') }}></script>
+    <script src={{ asset('./thedatepicker-master/dist/dataPicker.js') }}></script>
+    @vite(['resources/js/recommend.js'])
 @endsection
