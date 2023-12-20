28fccfa62618d321a851e9c84a6b5b9ee4932e24
CJ0423 <118946921+CJ0423@users.noreply.github.com>
2023-12-20 14:24:19 +0800

    3

--- resources/views/front/A1.blade.php
+++ resources/views/front/A1.blade.php
@@ -8,31 +8,26 @@
     {{-- <link rel="stylesheet" href="{{asset('css/a1.css')}}"> --}}
     @vite(['resources/css/front/a1.scss'])
     <style>
-        @media (max-width:767px) {
-            .a1-1 {
-                background-image: url("{{ asset('front-img/mainImg/a1/1-1.png') }}");
+        @foreach ($activities as $index => $item)
+            .a1-{{ $index + 1 }} {
+                background-image: url("{{ asset('storage/' . $item->img_pc_url) }}");
             }
 
-            .a1-2 {
-                background-image: url("{{ asset('front-img/mainImg/a1/2-2.png') }}");
-            }
+            @media (max-width:767px) {
+                .a1-{{ $index + 1 }} {
+                    background-image: url("{{ asset('storage/' . $item->img_pad_url) }}");
+                }
 
-            .a1-3 {
-                background-image: url("{{ asset('front-img/mainImg/a1/3-3.png') }}") !important;
             }
-
-            .a1-4 {
-                background-image: url("{{ asset('front-img/mainImg/a1/4-4.png') }}") !important;
-            }
-        }
+        @endforeach
     </style>
 @endsection
 
 @section('version')
     <div class="container-pc">
         @foreach ($activities as $index => $item)
-            <div data-aos="custom-animation-up" class="a{{ $index + 1 }}{{ $item->img_size_pad }} ">
-                <figure class="main-img a1-{{ $index }}">
+            <div data-aos="custom-animation-up" class="a{{ $index + 1 }} {{ $item->img_size_pad }} ">
+                <figure class="main-img a1-{{ $index + 1 }}">
                     <figcaption>
                         <h2>{{ $item->title }}</h2>
                         <h3>{{ $item->subtitle }}</h3>
