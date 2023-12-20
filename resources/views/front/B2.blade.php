a9029ba4b7b33c4eb116ef77a326c0255cd497f9
CJ0423 <118946921+CJ0423@users.noreply.github.com>
2023-12-20 14:39:49 +0800

    3

--- resources/views/front/B2.blade.php
+++ resources/views/front/B2.blade.php
@@ -5,80 +5,37 @@
 @endsection
 
 @section('css')
-@vite(['resources/css/front/b2.scss'])
-<style>
-    @media (max-width:767px) {
-        .a1-1 {
-            background-image: url("{{ asset('front-img/mainImg/a1/3-3.png') }}") !important;
-        }
+    @vite(['resources/css/front/b2.scss'])
+    <style>
+        @foreach ($activities as $index => $item)
+            .a1-{{ $index + 1 }} {
+                background-image: url("{{ asset('storage/' . $item->img_pc_url) }}");
+            }
 
-        .a1-2{
-            background-image: url("{{ asset('front-img/mainImg/a1/4-4.png') }}") !important;
-        }
-        .a1-3 {
-            background-image: url("{{ asset('front-img/mainImg/a1/3-3.png') }}") !important;
-        }
+            @media (max-width:767px) {
+                .a1-{{ $index + 1 }} {
+                    background-image: url("{{ asset('storage/' . $item->img_pad_url) }}");
+                }
 
-        .a1-4 {
-            background-image: url("{{ asset('front-img/mainImg/a1/4-4.png') }}") !important;
-        }
-        .a1-5 {
-            background-image: url("{{ asset('front-img/mainImg/a1/4-4.png') }}") !important;
-        }
-    }
-</style>
-
+            }
+        @endforeach
+    </style>
 @endsection
 
 @section('version')
     <div class="container-pc container-pc-b2">
-        <div data-aos="custom-animation-up" class="b1 row-img">
+        @foreach ($activities as $index => $item)
+            <div data-aos="custom-animation-up" class="b{{ $index + 1 }} {{ $item->img_size_pad }} ">
+                <figure class="main-img a1-{{ $index + 1 }}">
+                    <figcaption>
+                        <h2>{{ $item->title }}</h2>
+                        <h3>{{ $item->subtitle }}</h3>
+                        <a href="{{ $item->button_link }}" class="buy-now-button">{{ $item->button_name }}</a>
+                    </figcaption>
+                </figure>
+            </div>
+        @endforeach
 
-            <figure class="main-img a1-1"  style="background-image: url('{{ asset('front-img/mainImg/a1/1-1.png') }}');">
-                <figcaption>
-                    <h2>主標題文字1</h2>
-                    <h3>副標題文字1</h3>
-                    <a href="link-to-purchase-page" class="buy-now-button">按鍵名稱</a>
-                </figcaption>
-            </figure>
-        </div>
-        <div data-aos="custom-animation-up" class="b2 row-img">
-            <figure class="main-img a1-2" style="background-image: url('{{ asset('front-img/mainImg/a1/3-3.png') }}');">
-                <figcaption>
-                    <h2>主標題文字2</h2>
-                    <h3>副標題文字2</h3>
-                    <a href="link-to-purchase-page" class="buy-now-button">按鍵名稱</a>
-                </figcaption>
-
-            </figure>
-        </div>
-        <div data-aos="custom-animation-up" class="b3 row-img">
-            <figure class="main-img a1-3" style="background-image: url('{{ asset('front-img/mainImg/a1/4-4.png') }}');">
-                <figcaption>
-                    <h2>主標題文字3</h2>
-                    <h3>副標題文字3</h3>
-                    <a href="link-to-purchase-page" class="buy-now-button">按鍵名稱</a>
-                </figcaption>
-            </figure>
-        </div>
-        <div data-aos="custom-animation-up" class="b4 row-img">
-            <figure class="main-img a1-4" style="background-image: url('{{ asset('front-img/mainImg/a1/2-2.png') }}');">
-                <figcaption>
-                    <h2>主標題文字4</h2>
-                    <h3>副標題文字4</h3>
-                    <a href="link-to-purchase-page" class="buy-now-button">按鍵名稱</a>
-                </figcaption>
-            </figure>
-        </div>
-        <div data-aos="custom-animation-up" class="b5 row-img">
-            <figure class="main-img a1-5" style="background-image: url('{{ asset('front-img/mainImg/a1/2-2.png') }}');">
-                <figcaption>
-                    <h2>主標題文字5</h2>
-                    <h3>副標題文字5</h3>
-                    <a href="link-to-purchase-page" class="buy-now-button">按鍵名稱</a>
-                </figcaption>
-            </figure>
-        </div>
         <div style="clear: both"></div>
 
     </div>
