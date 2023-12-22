7cfe42532058b77a1165318a1b2baf589afb9f64
CJ0423 <118946921+CJ0423@users.noreply.github.com>
2023-12-22 21:43:58 +0800

    ˇ

--- routes/web.php
+++ routes/web.php
@@ -47,7 +47,7 @@ Route::get('/', function () {
             ->where('start_time', '<=', $now)
             ->orderBy('start_time', 'desc') // 確保最接近現在的記錄在最前面
             ->first(); // 只選擇一筆記錄
-        dd('錯誤');
+        // dd('錯誤');
     }
 
     $target = $results->whitch_pattern;
