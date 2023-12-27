3ebdddd1f53fe58f7aeede1590c6fdcdfafe4f1e
CJ0423 <118946921+CJ0423@users.noreply.github.com>
2023-12-27 15:25:23 +0800

    3

--- resources/views/front/A1.blade.php
+++ resources/views/front/A1.blade.php
@@ -22,19 +22,29 @@
         @endforeach
     </style>
 @endsection
+@php
+    $array=["patter-1","patter-2","patter-3","patter-4"]
+@endphp
+
+
 
 @section('version')
     <div class="container-pc">
         @foreach ($activities as $index => $item)
+
             <div data-aos="custom-animation-up" class="a{{ $index + 1 }} {{ $item->img_size_pad }} ">
+
                 <figure class="main-img a1-{{ $index + 1 }}">
-                    <figcaption>
-                        <h2>{{ $item->title }}</h2>
-                        <h3>{{ $item->subtitle }}</h3>
-                        <a href="{{ $item->button_link }}" class="buy-now-button">
-                            {{ $item->button_name }}
-                        </a>
-                    </figcaption>
+
+<div class="{{$array[$index]}}">
+                        <figcaption>
+                            <h2>{{ $item->title }}</h2>
+                            <h3>{{ $item->subtitle }}</h3>
+                            <a href="{{ $item->button_link }}" class="buy-now-button">
+                                {{ $item->button_name }}
+                            </a>
+                        </figcaption>
+</div>
                 </figure>
             </div>
         @endforeach
